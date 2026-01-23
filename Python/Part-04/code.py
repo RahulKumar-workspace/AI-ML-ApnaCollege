5af456272101d6a5dbd843fbe11056a0417e1a57
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-01-24 00:37:57 +0530

    basics of classes (types)

--- Python/Part-04/code.py
+++ Python/Part-04/code.py
@@ -5,3 +5,10 @@ class Student:
 stud1 = Student()
 stud2 = Student()
 print(stud1.subject,stud1.classno)
+
+l = [1,2]
+s = set()
+
+print(type(stud1))
+print(type(l)) #inbuild class in python
+print(type(s)) #inbuild class in python
